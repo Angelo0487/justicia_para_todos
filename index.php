20a3a86f15a2489b3a7da5d45218b5bc2aa33139
Angelo0487 <angelo0487@gmail.com>
2025-11-09 18:31:04 -0300

    pagina de inicio

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Inicio - Justicia para Todos</title>
+<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<h1>Justicia para Todos</h1>
+<div class="menu">
+  <a href="registro.php" class="boton">Registrar Cliente</a>
+  <a href="listar.php" class="boton">Ver Clientes</a>
+</div>
+</body>
+</html>
